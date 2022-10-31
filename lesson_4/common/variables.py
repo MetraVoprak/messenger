58acbf516179ac34c605aa117c67bebae661b821
MetraVoprak <84026187+MetraVoprak@users.noreply.github.com>
2022-10-31 13:24:33 +0300

    Lesson 4 (#4)

---
+++ lesson_4/common/variables.py
@@ -0,0 +1,22 @@
+
+# Порт по умолчанию
+DEFAULT_PORT = 7777
+# IP по умолчанию
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+# максимальная очередь
+MAX_CONNECT = 3
+# Максимальная длинна сообщения
+MAX_PACKAGE_LENGTH = 1024
+# Кодировка
+ENCODING = 'utf-8'
+
+# Протокол JIM, основные ключи
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+# Прочие ключи
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
